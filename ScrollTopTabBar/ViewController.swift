abb3113d04e5e1cb2b561a281ede53dc3078a424
Ragaie alfy <Ragaie@ehabs-bc-macbook-pro.local>
2018-01-17 16:11:53 +0200

    Initial Commit

---
+++ ScrollTopTabBar/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ScrollTopTabBar
+//
+//  Created by Ragaie alfy on 1/17/18.
+//  Copyright © 2018 Ragaie alfy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
